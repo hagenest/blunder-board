32a916129f9935ff25c92d152ca979f43c20e0d1
0x90.space <people@schleuder.0x90.space>
2021-08-04 23:05:23 +0100

    rate moves with stockfish-12

--- blunderboard.go
+++ blunderboard.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"github.com/notnil/chess"
 	"github.com/notnil/chess/uci"
-	"math/rand"
-	"time"
+	"math"
 )
+
+// stolen^H^H inspired from lichess https://github.com/ornicar/lila/blob/master/modules/analyse/src/main/Advice.scala#L79
+func WinningChance(cp int) float64 {
+	winning_chance := 2 / (1 + math.Exp(-0.004 * float64(cp))) - 1
+	return winning_chance
+}
 
 func main() {
 	engine, err := uci.New("stockfish")
@@ -19,19 +24,28 @@ func main() {
 		panic(err)
 	}
 
-	rand.Seed(time.Now().UnixNano())
-
-	// random vs stockfish
 	game := chess.NewGame()
 	for game.Outcome() == chess.NoOutcome {
-		moves := game.ValidMoves()
-		game.Move(moves[rand.Intn(len(moves))])
-
-		if err := engine.Run(uci.CmdPosition{Position: game.Position()}, uci.CmdGo{MoveTime: time.Second / 100}); err != nil {
+		if err := engine.Run(uci.CmdPosition{Position: game.Position()}, uci.CmdGo{Depth: 12}); err != nil {
 			panic(err)
 		}
-		game.Move(engine.SearchResults().BestMove)
+		search_results := engine.SearchResults()
+		fmt.Println("Best Move: ", search_results.BestMove)
+		cp := search_results.Info.Score.CP
+		fmt.Println("Score (centipawns): ", cp)
+		winning_chance := WinningChance(cp)
+		fmt.Println("Winning chance: ", winning_chance)
+		fmt.Println(game.Position().Board().Draw())
+		for {
+			var move string
+			fmt.Print("Move: ")
+			fmt.Scanln(&move)
+			if err := game.MoveStr(move); err == nil {
+				break
+			}
+			fmt.Println("Illegal move!")
+		}
 	}
-	fmt.Println(game.String())
+	fmt.Println(game.Outcome())
 	fmt.Println(game.Position().Board().Draw())
 }
